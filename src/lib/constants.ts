54782ab20081d390f14db71b63e4e660b9cf7eb9
Botirjon777 <stev6jobs777@gmail.com>
2026-04-23 16:33:05 +0500

    feat: add project requirement documentation and initialize configuration constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://test.qadam.services/api/v1/";
+  process.env.NEXT_PUBLIC_API_URL || "https://qadam.services/api/v1/";
 
 export const GOOGLE_MAPS_API_KEY =
   process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
